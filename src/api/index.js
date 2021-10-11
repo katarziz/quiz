c1a6e12d13ba0fc0867ce6265c47871a22048004
Marius Winkler <marschi10000@googlemail.com>
2021-10-11 21:25:53 +0200

    implemented docker

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://91.10.201.75:5000/entries';
+const url = 'http://localhost:5000/entries';
 
 export const fetchEntries = () => axios.get(url);
 export const createEntry = (newEntry) => axios.post(url, newEntry);
