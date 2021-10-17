050c2ad079d5d949db26e97d3ad707d0c960a7f7
Marius Winkler <marschi10000@googlemail.com>
2021-10-17 19:59:07 +0200

    resolved .env troubles

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4999/entries';
+const url = 'http://91.9.65.139:4999/entries';
 
 export const fetchEntries = () => axios.get(url);
 export const createEntry = (newEntry) => axios.post(url, newEntry);
