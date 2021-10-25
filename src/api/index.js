f953e474ca030cb6d03b94b1e5685b6eda47d417
Marius Winkler <marschi10000@googlemail.com>
2021-10-25 15:38:39 +0200

    ip update

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://91.9.65.139:4999/entries';
+const url = 'http://91.9.66.249:4999/entries';
 
 export const fetchEntries = () => axios.get(url);
 export const createEntry = (newEntry) => axios.post(url, newEntry);
